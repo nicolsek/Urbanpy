1a6a817da7530bc25c7c287faeab5d228b2b9540
nicolsek <tusek.nicole@gmail.com>
2018-03-15 03:21:47 -0700

    Finished functions and input.

--- urband.py
+++ urband.py
@@ -3,8 +3,8 @@ import requests # For requesting the appropriate API stuff.
 
 # -- Global Vars --
 
-failedArg = 'Failed to parse argument(s).'
 failedReq = 'Could not get response from Urban Dictionary API.'
+noDefs = 'No definitions could be found.'
 
 reqLink = 'http://api.urbandictionary.com/v0/define?term='
 
@@ -12,16 +12,50 @@ def quit(msg):
     print('[!] ' + msg)
     sys.exit(1)
 
-def reqAPI(word):
-    resp = requests.get(reqLink + word)
+def getWord():
+    word = ''
     
-try:
-    global word 
-    word = sys.argv[1]
-except:
-    quit(failedArg)
+    for words in sys.argv[1:]:
+        word += words + ' '
 
-try:
-    reqAPI(word)
-except:
-    quit(failedReq) 
+    word.strip()
+
+    return word
+
+def reqAPI(word):
+    global resp
+    resp = requests.get(reqLink + word)
+
+def parseResp(resp):
+    sizeOfDefs = len(resp.json()['list'])
+   
+    if (sizeOfDefs < 1):
+        quit(noDefs)
+
+    numOfDefs = min(sizeOfDefs, 3)
+
+    keys = ['author', 'definition', 'example']
+
+    for defs in range(0, numOfDefs):
+        for key in keys:
+            val = resp.json()['list'][defs][key]
+            
+            if (key == 'author'):
+                print('Author --> ' + val)
+            elif (key == 'definition'):
+                print('Def --> ' + val)
+            elif (key == 'example'):
+                print('Example --> ' + val)
+        
+        print('\n')
+
+
+if __name__ == "__main__":
+    word = getWord()
+
+    try:
+        reqAPI(word)
+    except:
+        quit(failedReq) 
+
+    parseResp(resp)
